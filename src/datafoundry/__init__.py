245f8521880c8ece16fab6552300374559c5fea9
jchharrell <52176964+jchharrell@users.noreply.github.com>
2026-09-11 22:26:14 -0400

    Export DataFoundry contracts and quality gate

--- src/datafoundry/__init__.py
+++ src/datafoundry/__init__.py
@@ -1,4 +1,18 @@
 from .pipeline import CleaningPipeline, CleaningReport
 from .rules import normalize_email, normalize_state, valid_age
+from .contracts import ColumnContract, ContractViolation, DataContract, customer_contract
+from .quality_gate import LineageReceipt, apply_quality_gate
 
-__all__ = ["CleaningPipeline", "CleaningReport", "normalize_email", "normalize_state", "valid_age"]
+__all__ = [
+    "CleaningPipeline",
+    "CleaningReport",
+    "normalize_email",
+    "normalize_state",
+    "valid_age",
+    "ColumnContract",
+    "ContractViolation",
+    "DataContract",
+    "customer_contract",
+    "LineageReceipt",
+    "apply_quality_gate",
+]
